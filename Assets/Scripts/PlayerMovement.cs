58f616b8d50f6ecdd9a79aa83b61a59efdc073ed
AdrielABS <adriel.bsilva1234@gmail.com>
2026-06-04 17:23:03 -0300

    Conserto e separação
    Conserto das linhas de código do respawn e separação dos arquivos de movimento do player e seus respawns.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,25 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 8f;
 
-    [Header("Respawn Settings")]
-    public Vector3 respawnPoint;
-    
-
     private Collider2D playerCollider;
     private Collider2D currentPlatform;
-    
     private Rigidbody2D rb;
+
     private bool isGrounded;
-    private bool fallen = false;
 
-    IEnumerator DropThroughPlatform()
-    {
-        Physics2D.IgnoreCollision(playerCollider, currentPlatform, true);
-        
-        yield return new WaitForSeconds(0.5f);
-
-        Physics2D.IgnoreCollision(playerCollider, currentPlatform, false);
-    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,18 +21,10 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
-    {        
+    {
         Move();
         Jump();
         DropThrough();
-    if (fallen)
-        {
-            if (Keyboard.current.rKey.wasPressedThisFrame)
-            {
-                ReturnToSpawnPoint();
-                fallen = false;
-            }
-        }
     }
 
     void Move()
@@ -55,60 +34,71 @@ public class PlayerMovement : MonoBehaviour
         if (Keyboard.current.aKey.isPressed)
         {
             move = -1f;
-            if(Keyboard.current.shiftKey.isPressed)
+
+            if (Keyboard.current.shiftKey.isPressed)
                 move = -1.5f;
         }
+
         if (Keyboard.current.dKey.isPressed)
         {
             move = 1f;
-            if(Keyboard.current.shiftKey.isPressed)
+
+            if (Keyboard.current.shiftKey.isPressed)
                 move = 1.5f;
         }
-        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        rb.linearVelocity =
+            new Vector2(move * speed, rb.linearVelocity.y);
     }
+
     void Jump()
     {
-        if(Keyboard.current.wKey.wasPressedThisFrame && isGrounded)
+        if (Keyboard.current.wKey.wasPressedThisFrame && isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            rb.linearVelocity =
+                new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
+
     void DropThrough()
     {
-        if (Keyboard.current.sKey.wasPressedThisFrame && currentPlatform != null)
+        if (Keyboard.current.sKey.wasPressedThisFrame &&
+            currentPlatform != null)
         {
             StartCoroutine(DropThroughPlatform());
         }
     }
 
+    IEnumerator DropThroughPlatform()
+    {
+        Physics2D.IgnoreCollision(
+            playerCollider,
+            currentPlatform,
+            true);
 
-    private void ReturnToSpawnPoint()
-    {
-        transform.position = new Vector3(respawnPoint.x, respawnPoint.y, transform.position.z);
-        rb.linearVelocity = Vector2.zero;
+        yield return new WaitForSeconds(0.5f);
+
+        Physics2D.IgnoreCollision(
+            playerCollider,
+            currentPlatform,
+            false);
     }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("FallZone"))
-        {
-            fallen = true;
-            Debug.Log("Você caiu!");
-        }
-    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         isGrounded = true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    if (collision.gameObject.CompareTag("Platform")){
-            currentPlatform = collision.collider;
-        }
-    }
-
     private void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            currentPlatform = collision.collider;
+        }
     }
 }
